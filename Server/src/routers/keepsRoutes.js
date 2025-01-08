3f27efb6cfa09462dba12fb66bf869580fbcc78b
yoavi1 <yoavmeirovitz187@gmail.com>
2025-01-08 21:03:43 +0200

    now we can create keep with title and post it to the database

--- Server/src/routers/keepsRoutes.js
+++ Server/src/routers/keepsRoutes.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Keep = require('../models/Keep.model');
 
-router.get('/', (req, res) => {
-  res.json({ name: 'yoav' });
+router.get('/', async (req, res) => {
+  try {
+    const keeps = await Keep.find();
+    res.json(keeps);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const keep = new Keep(req.body);
+    await keep.save();
+    res.json({ keep });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 module.exports = router;
