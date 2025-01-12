66359748da1068fba68220debc64318708bda806
yoavi1 <yoavmeirovitz187@gmail.com>
2025-01-12 17:48:56 +0200

    clean the code a little bit

--- Server/src/models/hey.js
+++
@@ -1,6 +0,0 @@
-// const mongoose = require('mongoose');
-// import { Keep } from './Keep.model';
-// const theNewKeepsSchema = Keep.discriminator(
-//   'hey',
-//   new mongoose.Schema({ pin: Boolean }, options)
-// );
