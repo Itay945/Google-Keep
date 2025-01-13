8af302bf59a7dfdc0bdffba04ab6ccccfb84237f
Itay Benadon <itay945@yahoo.com>
2025-01-13 13:03:42 +0200

    fixed coloring, added connection between back and front with get all keeps and verious css changes

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,9 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      greySearchBarColor: "#F1F3F4",
-    },
+    // colors: {
+    //   greySearchBarColor: "#F1F3F4",
+    //   white: "#FFFFFF",
+    // },
 
     extend: {},
   },
