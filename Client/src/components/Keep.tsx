91d84dcc6031f39b0c005cf977a405f7628196ce
Itay Benadon <itay945@yahoo.com>
2025-01-15 13:15:08 +0200

    REMEMBER TO CHANGE COMPONENT NAMES

--- Client/src/components/Keep.tsx
+++ Client/src/components/Keep.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SingleKeep from "../components/SingleKeep";
+import SingleKeep from "./SingleKeep";
 
 export default function Keep() {
   type Keep = {
