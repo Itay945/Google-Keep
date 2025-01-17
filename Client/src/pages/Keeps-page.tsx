8dde767127c3aae2195223be5a81db12797a024b
yoavi1 <yoavmeirovitz187@gmail.com>
2025-01-17 22:36:30 +0200

    no real change

--- Client/src/pages/Keeps-page.tsx
+++ Client/src/pages/Keeps-page.tsx
@@ -1,5 +1,5 @@
-import NoteForm from "../components/Input-Form";
-import Keep from "../components/KeepMain";
+import NoteForm from '../components/Input-Form';
+import Keep from '../components/KeepMain';
 export default function KeepsPage() {
   return (
     <div className="flex justify-center flex-col items-center">
