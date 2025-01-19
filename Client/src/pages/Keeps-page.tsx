1dd010f5092ddc7601e927971876d5864e8aad0f
Itay Benadon <itay945@yahoo.com>
2025-01-19 16:55:43 +0200

    changed keepmain name in Keeps-page for a more fitting name

--- Client/src/pages/Keeps-page.tsx
+++ Client/src/pages/Keeps-page.tsx
@@ -1,10 +1,10 @@
-import NoteForm from '../components/Input-Form';
-import Keep from '../components/KeepMain';
+import NoteForm from "../components/Input-Form";
+import AllKeeps from "../components/KeepMain";
 export default function KeepsPage() {
   return (
     <div className="flex justify-center flex-col items-center">
       <NoteForm />
-      <Keep />
+      <AllKeeps />
     </div>
   );
 }
