c1fde466f673b7d4c170b4c08d670a585f6d72a3
Itay Benadon <itay945@yahoo.com>
2025-01-21 12:13:57 +0200

    moved three dots position in the repository to single keep icons folder

--- Client/src/components/SingleKeep.tsx
+++ Client/src/components/SingleKeep.tsx
@@ -1,42 +1,42 @@
 // SingleKeep.tsx
-import plusBell from './../assets/add_alert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import collaborator from './../assets/person_add_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import colors from './../assets/palette_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import brush from './../assets/brush_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import archive from './../assets/archive_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import threeDots from './../assets/more_vert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import pin from './../assets/keep_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg';
-import ColorPicker from './Single-Keep-icons/ColorPicker';
-import DropDownThreeDots from './icon-components/ThreeDotsDropDown';
-import circularV from '../assets/check_circle_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg';
-import { useState } from 'react';
-import { Keep, KeepColor } from './KeepMain';
+import plusBell from "./../assets/add_alert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import collaborator from "./../assets/person_add_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import colors from "./../assets/palette_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import brush from "./../assets/brush_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import archive from "./../assets/archive_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import threeDots from "./../assets/more_vert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import pin from "./../assets/keep_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.svg";
+import ColorPicker from "./Single-Keep-icons/ColorPicker";
+import DropDownThreeDots from "./Single-Keep-icons/ThreeDotsDropDown";
+import circularV from "../assets/check_circle_24dp_000000_FILL1_wght400_GRAD0_opsz24.svg";
+import { useState } from "react";
+import { Keep, KeepColor } from "./KeepMain";
 
 const colorMap: Record<KeepColor, string> = {
-  Coral: '#FAAFA8',
-  Peach: '#F39F76',
-  Sand: '#FFF8B8',
-  Mint: '#E2F6D3',
-  Sage: '#B4DDD3',
-  Fog: '#D4E4ED',
-  Storm: '#AECCDC',
-  Dusk: '#D3BFDB',
-  Blossom: '#F6E2DD',
-  Clay: '#E9E3D4',
-  Chalk: '#EFEFF1',
+  Coral: "#FAAFA8",
+  Peach: "#F39F76",
+  Sand: "#FFF8B8",
+  Mint: "#E2F6D3",
+  Sage: "#B4DDD3",
+  Fog: "#D4E4ED",
+  Storm: "#AECCDC",
+  Dusk: "#D3BFDB",
+  Blossom: "#F6E2DD",
+  Clay: "#E9E3D4",
+  Chalk: "#EFEFF1",
 };
 const darkModeColorMap: Record<KeepColor, string> = {
-  Coral: '#77172E',
-  Peach: '#692B17',
-  Sand: '##7C4A03',
-  Mint: '#264D3B',
-  Sage: '#0C625D',
-  Fog: '#256377',
-  Storm: '#284255',
-  Dusk: '#472E5B',
-  Blossom: '#6C394F',
-  Clay: '#4B443A',
-  Chalk: '#232427',
+  Coral: "#77172E",
+  Peach: "#692B17",
+  Sand: "##7C4A03",
+  Mint: "#264D3B",
+  Sage: "#0C625D",
+  Fog: "#256377",
+  Storm: "#284255",
+  Dusk: "#472E5B",
+  Blossom: "#6C394F",
+  Clay: "#4B443A",
+  Chalk: "#232427",
 };
 
 type KeepProps = {
@@ -52,16 +52,9 @@ export default function SingleKeep({ keep }: KeepProps) {
   };
   return (
     <>
-      <div
-        className=" rounded-lg p-4 group hover:shadow-[0_0_4px_rgb(0,0,0,0.3)] "
-        style={{ backgroundColor: colorMap[currentColor] || '#ffffff' }}
-      >
+      <div className=" rounded-lg p-4 group hover:shadow-[0_0_4px_rgb(0,0,0,0.3)] " style={{ backgroundColor: colorMap[currentColor] || "#ffffff" }}>
         <div className="flex justify-between">
-          <h3
-            className={`text-lg font-bold ${keep.pin ? 'text-yellow-500' : ''}`}
-          >
-            {keep.title}
-          </h3>
+          <h3 className={`text-lg font-bold ${keep.pin ? "text-yellow-500" : ""}`}>{keep.title}</h3>
           <img
             src={pin}
             alt="pin"
@@ -70,7 +63,7 @@ export default function SingleKeep({ keep }: KeepProps) {
         </div>
         <p className="text-sm">{keep.description}</p>
         <p className="text-xs">
-          <strong>Labels:</strong> {keep.labels.join(', ')}
+          <strong>Labels:</strong> {keep.labels.join(", ")}
         </p>
         <p className="text-xs">
           <small>{new Date(keep.createdAt).toLocaleDateString()}</small>
