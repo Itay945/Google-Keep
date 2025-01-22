4f25a33b5d8684a58934311b0b31b8b55ff9c20d
yoavi1 <yoavmeirovitz187@gmail.com>
2025-01-22 13:51:56 +0200

    now you can create user

--- Client/src/pages/BinPage.tsx
+++ Client/src/pages/BinPage.tsx
@@ -1,6 +1,23 @@
-import SingleKeepBinned from '../components/SingleKeepBinned';
+import { useEffect, useState } from 'react';
+// import SingleKeepBinned from '../components/SingleKeepBinned';
+import api from '../helpers/axiosApiToken';
 
 export default function BinPage() {
+  const [keepsBined, setkeepsBined] = useState();
+  useEffect(() => {
+    const fetchKeeps = async () => {
+      try {
+        const response = await api.get('/users/trash');
+        console.log('response: ', response.data);
+        setkeepsBined(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchKeeps();
+  }, []);
+
   return (
     <>
       <div className="flex justify-center">
@@ -8,6 +25,9 @@ export default function BinPage() {
         <button>Empty bin</button>
       </div>
       {/* <SingleKeepBinned /> */}
+      {keepsBined.map((item) => {
+        return <div>{item}</div>;
+      })}
     </>
   );
 }
--- Server/src/controllers/userController.js
+++ Server/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User.model');
+const Keep = require('../models/Keep.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
