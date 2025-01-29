aa3fb02b0cf3435d8013f66d86963c08e150feed
Itay Benadon <itay945@yahoo.com>
2025-01-29 15:04:48 +0200

    please work too many changes to count thank you omer mezig.js

--- Client/src/components/BinMain.tsx
+++ Client/src/components/BinMain.tsx
@@ -1,5 +1,5 @@
 // import SingleKeep from './SingleKeep';
-import SingleTrash from './SingleTrash';
+import SingleTrash from "./SingleTrash";
 
 export type Keep = {
   _id: string;
@@ -13,28 +13,20 @@ export type Keep = {
   author: string;
   pin: boolean;
 };
-export type KeepColor =
-  | 'Transparent'
-  | 'Coral'
-  | 'Peach'
-  | 'Sand'
-  | 'Mint'
-  | 'Sage'
-  | 'Fog'
-  | 'Storm'
-  | 'Dusk'
-  | 'Blossom'
-  | 'Clay'
-  | 'Chalk';
+export type KeepColor = "Transparent" | "Coral" | "Peach" | "Sand" | "Mint" | "Sage" | "Fog" | "Storm" | "Dusk" | "Blossom" | "Clay" | "Chalk";
+type BinMainProps = {
+  keeps: Keep[];
+  fetchKeeps: () => Promise<void>;
+};
 
-export default function BinMain({ keeps }) {
+export default function BinMain({ keeps, fetchKeeps }: BinMainProps) {
   return (
     <>
       <div className="flex flex-wrap gap-4 p-4">
         <div className="">
           <div className="flex flex-wrap gap-4 p-4">
             {keeps.map((keep) => (
-              <SingleTrash key={keep._id} keep={keep} />
+              <SingleTrash key={keep._id} keep={keep} fetchKeeps={fetchKeeps} />
             ))}
           </div>
         </div>
--- Client/src/pages/BinPage.tsx
+++ Client/src/pages/BinPage.tsx
@@ -1,34 +1,35 @@
 // import KeepsMain from '../components/KeepsMain';
-import { useState, useEffect } from 'react';
-import api from '../helpers/axiosApiToken';
-import { useAuth } from '../hooks/useAuth';
-import BinMain from '../components/BinMain';
+import { useState, useEffect } from "react";
+import api from "../helpers/axiosApiToken";
+import { useAuth } from "../hooks/useAuth";
+import BinMain from "../components/BinMain";
+import { Keep } from "../components/KeepsMain";
 function BinPage() {
   const { loggedInUser } = useAuth();
-  console.log('loggedInUser from useHuth: ', loggedInUser);
+  console.log("loggedInUser from useHuth: ", loggedInUser);
 
   const [keeps, setKeeps] = useState<Keep[]>([]);
 
   useEffect(() => {
-    const fetchKeeps = async () => {
-      try {
-        if (!loggedInUser) {
-          return;
-        }
-        const response = await api.get(`/keeps/trash`);
-        console.log('response: ', response.data.data.keeps);
-        setKeeps(response.data.data.keeps);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
     fetchKeeps();
   }, [loggedInUser]);
 
+  const fetchKeeps = async () => {
+    try {
+      if (!loggedInUser) {
+        return;
+      }
+      const response = await api.get(`/keeps/trash`);
+      console.log("response: ", response.data.data.keeps);
+      setKeeps(response.data.data.keeps);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="flex justify-center flex-col items-center">
-      <BinMain keeps={keeps} />
+      <BinMain keeps={keeps} fetchKeeps={fetchKeeps} />
     </div>
   );
 }
