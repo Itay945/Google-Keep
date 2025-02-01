2c095739065fc54e05025c776d4982dd57ae590c
yoavi1 <yoavmeirovitz187@gmail.com>
2025-02-01 13:14:15 +0200

    sut

--- Client/src/pages/Keeps-page.tsx
+++ Client/src/pages/Keeps-page.tsx
@@ -5,27 +5,28 @@ import KeepsForm from '../components/KeepsForm';
 import KeepsMain, { Keep } from '../components/KeepsMain';
 import api from '../helpers/axiosApiToken';
 import { useAuth } from '../hooks/useAuth';
-import { Outlet  useLocation} from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export default function KeepsPage() {
   const { loggedInUser } = useAuth();
-  console.log("loggedInUser from useHuth: ", loggedInUser);
+  console.log('loggedInUser from useHuth: ', loggedInUser);
 
   const [keeps, setKeeps] = useState<Keep[]>([]);
   const [error, setError] = useState<string | null>(null);
   const location = useLocation();
   useEffect(() => {
     // to do: fetch again when coming back from KeepDetails
+
     const fetchKeeps = async () => {
       try {
         if (!loggedInUser) {
           return;
         }
         const response = await api.get(`/keeps/user/${loggedInUser.userId}`);
-        console.log("response: ", response.data.data.keeps);
+        console.log('response: ', response.data.data.keeps);
         setKeeps(response.data.data.keeps);
       } catch (err) {
-        setError("Failed to fetch keeps.");
+        setError('Failed to fetch keeps.');
         console.error(err);
       }
     };
@@ -34,7 +35,11 @@ export default function KeepsPage() {
   }, [loggedInUser, location.pathname]);
 
   const handleKeepUpdate = (keepId: string, updates: Partial<Keep>) => {
-    setKeeps((prevKeeps) => prevKeeps.map((keep) => (keep._id === keepId ? { ...keep, ...updates } : keep)));
+    setKeeps((prevKeeps) =>
+      prevKeeps.map((keep) =>
+        keep._id === keepId ? { ...keep, ...updates } : keep
+      )
+    );
   };
   const handleKeepAdded = (newKeep: Keep) => {
     setKeeps((prevKeeps) => [newKeep, ...prevKeeps]);
